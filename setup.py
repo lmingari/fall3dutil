2ddf2b335c28a6ddd7b2fa39321fb28a6d6bfe79
Leonardo A. Mingari <lmingari@gmail.com>
2019-11-07 14:05:07 +0100

    name changed to fall3dutils

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="fall3d-metpy", 
+    name="fall3dutils", 
     version="1.0",
     author="Leonardo Mingari",
     author_email="lmingari@gmail.com",
-    description="Met utilities for the FALL3D model",
+    description="Utilities for the FALL3D model",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/lmingari/fall3d-metpy",
+    url="https://github.com/lmingari/fall3dutils",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",

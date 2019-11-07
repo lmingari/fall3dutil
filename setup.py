2ba21d256917a2de1e30b3e5122c84771865783d
Leonardo A. Mingari <lmingari@gmail.com>
2019-11-07 14:24:03 +0100

    change pre-requisites

--- setup.py
+++ setup.py
@@ -13,6 +13,15 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/lmingari/fall3dutils",
     packages=setuptools.find_packages(),
+    install_requires=[
+        'cdsapi',
+        'netcdf4',
+        'configparser',
+        'numpy',
+        'requests',
+        'datetime',
+        'argparser'
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
